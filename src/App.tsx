c8d3c1c7f008685aaedc01982ee483d03cb71dc1
arunprasath13 <your-email@example.com>
2025-03-18 18:12:01 +0530

    Fixed bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import Forms from './components/Forms'
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 function App() {
   
 
